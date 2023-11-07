e8ae1778b7cbf318038b65d8a453870148c3d0a8
Roovelo <rnjs8657@naver.com>
2023-11-07 17:44:33 +0900

    Update create-song-table.js

--- api/create-song-table.js
+++ api/create-song-table.js
@@ -5,7 +5,7 @@ export default async function handler(
   response: VercelResponse,
 ){
   try{
-    const result = await sql'CREATE TABLE Songs (Number varchar(255), Name varchar(255), Link varchar(255), File varchar(255) );';
+    const result = await sql'CREATE TABLE Songs (SongNumber varchar(255), Name varchar(255), Link varchar(255), FileURL varchar(255) );';
     return response.status(200).json({result});
   } catch (error){
     return response.status(500).jsoin({error});
